dd0ba5f60460f25b0d65bb279ebdb5f93065ecb6
Halban <30965946+Halbann@users.noreply.github.com>
2026-04-20 11:51:39 +0100

    Bump version to 1.1.0

--- Solution/Source/Properties/AssemblyVersion.cs
+++ Solution/Source/Properties/AssemblyVersion.cs
@@ -5,6 +5,6 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.0.2.0")]
- [assembly: AssemblyFileVersion("1.0.2.0")]
- [assembly: KSPAssembly("Stapler", 1, 0, 2)]
+ [assembly: AssemblyVersion("1.1.0.0")]
+ [assembly: AssemblyFileVersion("1.1.0.0")]
+ [assembly: KSPAssembly("Stapler", 1, 1, 0)]
